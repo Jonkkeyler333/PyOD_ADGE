4b52e5b91496aa36415e7b3ee90c4eecaf730e8c
Jonkkeyler333 <82979079+Jonkkeyler333@users.noreply.github.com>
2025-04-27 17:07:10 -0500

    create the Base for the models

--- PyOD_ADGE/models/base.py
+++ PyOD_ADGE/models/base.py
@@ -1,1 +1,19 @@
-import this
+from abc import ABC,abstractmethod
+from typing import Union
+
+class BaseDetector(ABC):
+    """_summary_
+    """
+    def __init__(self,contamination:Union[float,int]=0.1):
+        """Constructor for BaseDetector class.
+
+        :param contamination: The amount of outliers expected in the data, defaults to 0.1
+        :type contamination: float, optional
+        :raises ValueError: _description_
+        """
+        if isinstance(contamination,(int,float)):
+            if not (0<contamination<0.5): 
+                raise ValueError("contamination must be between 0 and 0.5") #Si es mayor a 0.5 no tiene sentido
+            else:
+                self.contamination = contamination
+    
