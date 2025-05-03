f7904cafb54e2ccf7f2bca3dbfab3a767b6eb4b0
Jonkkeyler333 <82979079+Jonkkeyler333@users.noreply.github.com>
2025-05-03 17:04:56 -0500

    LOF V2 , with some test

---
+++ PyOD_ADGE/models/__init__.py
@@ -0,0 +1,4 @@
+from .local_outlier_factor import LocalOutlierFactor
+from .base import BaseDetector
+
+__all__ = ["LocalOutlierFactor", "BaseDetector"]
--- PyOD_ADGE/models/local_outlier_factor.py
+++ PyOD_ADGE/models/local_outlier_factor.py
@@ -4,38 +4,49 @@ import scipy.spatial.distance
 import numpy as np
 
 class LocalOutlierFactor(BaseEstimator, OutlierMixin):
-    def __init__(self,k_neighbors:int=20,metric:str='euclidean'):
+    def __init__(self,k_neighbors:int=20,metric:str='euclidean',contamination:float=0.1,**metric_params):
+        if k_neighbors<1:
+            raise ValueError("k_neighbors must be greater than 0")
+        if contamination<0 or contamination>0.5:
+            raise ValueError("contamination must be between 0 and 0.5")
         self.k_neighbors = k_neighbors
         self.metric = metric
+        self.metric_params = metric_params
+        self.contamination = contamination
         self._k_distances_cache = {}
                 
     def fit(self,X,y=None):
         self._X=X
+        if X.shape[0]<self.k_neighbors:
+            raise ValueError("The number of samples must be greater than k_neighbors")
         self.metric_c = self._get_distance_metric()
         self._k_distances_cache = self._get_all_k_distances(self._X)
         self.decision_scores_=self._local_outlier_factor(self._X)
         self._is_fitted=True
         return self
     
-    def decision_function(self,X):
+    def decision_function(self):
         check_is_fitted(self, '_is_fitted')
         if not hasattr(self, '_X'):
             raise ValueError("The model has not been fitted yet.")
-        if X.shape[1] != self._X.shape[1]:
-            raise ValueError("The input data must have the same number of features as the training data.")
-        if X.shape != self._X.shape or not np.allclose(X, self._X):
-            raise NotImplementedError(
-                "decision_function currently only supports training data."
-            )
-        return self.decision_scores_
+        scores=self.outlier_factor_
+        self.threshold_ = np.quantile(scores, 1 - self.contamination)
+        return np.where(scores>self.threshold_,-1,1)
+    
+    @property
+    def outlier_factor_(self):
+        check_is_fitted(self, '_is_fitted')
+        sorted_scores = sorted(self.decision_scores_, key=lambda x: x[0])
+        return np.array([score[1] for score in sorted_scores])
     
     def _get_distance_metric(self):
+            if self.metric == 'minkowski':
+                return lambda u, v: scipy.spatial.distance.minkowski(u, v, **self.metric_params)
             metrics = {
             'euclidean': scipy.spatial.distance.euclidean,
             'manhattan': scipy.spatial.distance.cityblock,
             'cosine': scipy.spatial.distance.cosine,
             'chebyshev': scipy.spatial.distance.chebyshev,
-            'minkowski': scipy.spatial.distance.minkowski,
             }
             try:
                 return metrics[self.metric]
@@ -75,7 +86,11 @@ class LocalOutlierFactor(BaseEstimator, OutlierMixin):
                 o = D[idx_o]
                 o_k_distance = k_distances[idx_o][0]
                 reach_distances.append(self._reach_dist(p,o,o_k_distance))
-            lrd.append((idx_p,1/(sum(reach_distances)/len(k_neighbors_p))))
+            rd=np.array(reach_distances)
+            srd=np.sum(rd)
+            if srd==0:
+                srd=np.inf
+            lrd.append((idx_p,1/(srd/len(k_neighbors_p))))
         return lrd
             
     def _local_outlier_factor(self,D):
@@ -86,6 +101,10 @@ class LocalOutlierFactor(BaseEstimator, OutlierMixin):
             lrd_p = lrd[idx_p][1]
             sum_lrd = 0
             for idx_o in k_neighbors_p:
-                sum_lrd += lrd[idx_o][1]/lrd_p
+                if np.isinf(lrd_p):
+                    ratio = 1 if not np.isinf(lrd[idx_o][1]) else np.inf
+                    sum_lrd += ratio
+                else:
+                    sum_lrd += lrd[idx_o][1]/lrd_p
             lof.append((idx_p,sum_lrd/len(k_neighbors_p)))
         return lof
