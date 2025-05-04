2c7e3ccb9faab4039d05b8f339ae5fccdb2d7200
Jonkkeyler333 <82979079+Jonkkeyler333@users.noreply.github.com>
2025-05-03 22:42:17 -0500

    DOC of LOF

--- PyOD_ADGE/models/local_outlier_factor.py
+++ PyOD_ADGE/models/local_outlier_factor.py
@@ -4,7 +4,22 @@ import scipy.spatial.distance
 import numpy as np
 
 class LocalOutlierFactor(BaseEstimator, OutlierMixin):
+    """Local Outlier Factor (LOF) algorithm for outlier detection.
+    This class implements the LOF algorithm, which identifies outliers in a dataset based on the local density of data points.
+    """
     def __init__(self,k_neighbors:int=20,metric:str='euclidean',contamination:float=0.1,**metric_params):
+        """The constructor for LocalOutlierFactor class.
+        :param k_neighbors: The number of neighbors to use for the LOF calculation, defaults to 20
+        :type k_neighbors: int, optional
+        :param metric: The distance metric to use, defaults to 'euclidean'
+        :type metric: str, optional
+        :param contamination: The proportion of outliers in the data, defaults to 0.1
+        :type contamination: float, optional
+        :param metric_params: Additional parameters for the distance metric, defaults to {}
+        :type metric_params: dict, optional
+        :raises ValueError: If k_neighbors is less than 1 or if contamination is not between 0 and 0.5
+        :raises ValueError: If the metric is not recognized
+        """
         if k_neighbors<1:
             raise ValueError("k_neighbors must be greater than 0")
         if contamination<0 or contamination>0.5:
@@ -16,6 +31,16 @@ class LocalOutlierFactor(BaseEstimator, OutlierMixin):
         self._k_distances_cache = {}
                 
     def fit(self,X,y=None):
+        """fit the model to the data.
+
+        :param X: The input data, a 2D array-like structure where each row is a sample and each column is a feature.
+        :type X: array-like, shape (n_samples, n_features)
+        :param y: This parameter isn't used in this implementation, but it's included for compatibility with scikit-learn's fit method.
+        :type y: None
+        :raises ValueError: If the number of samples is less than k_neighbors.
+        :return: self
+        :rtype: LocalOutlierFactor
+        """
         self._X=X
         if X.shape[0]<self.k_neighbors:
             raise ValueError("The number of samples must be greater than k_neighbors")
@@ -34,26 +59,50 @@ class LocalOutlierFactor(BaseEstimator, OutlierMixin):
         return np.where(scores>self.threshold_,-1,1)
     
     @property
-    def outlier_factor_(self):
+    def outlier_factor_(self)->np.ndarray:
+        """Returns the outlier factor for each sample in the dataset.
+        The outlier factor is a measure of how much a sample deviates from its neighbors.
+
+        :return: LOF scores for each sample in the dataset.
+        :rtype: numpy.ndarray, shape (n_samples,)
+        :raises ValueError: If the model has not been fitted yet.
+        """
         check_is_fitted(self, '_is_fitted')
         sorted_scores = sorted(self.decision_scores_, key=lambda x: x[0])
         return np.array([score[1] for score in sorted_scores])
     
     def _get_distance_metric(self):
-            if self.metric == 'minkowski':
-                return lambda u, v: scipy.spatial.distance.minkowski(u, v, **self.metric_params)
-            metrics = {
+        """Get the distance metric from scipy.spatial.distance based on the metric name.
+
+        :raises ValueError: If the metric is not recognized.
+        :return: Distance function from scipy.spatial.distance.
+        :rtype: scipy.spatial.distance function
+        """
+        if self.metric == 'minkowski':
+            return lambda u, v: scipy.spatial.distance.minkowski(u, v, **self.metric_params)
+        metrics = {
             'euclidean': scipy.spatial.distance.euclidean,
             'manhattan': scipy.spatial.distance.cityblock,
             'cosine': scipy.spatial.distance.cosine,
             'chebyshev': scipy.spatial.distance.chebyshev,
-            }
-            try:
-                return metrics[self.metric]
-            except KeyError:
-                raise ValueError(f"Unknown metric: {self.metric}.")
+        }
+        try:
+            return metrics[self.metric]
+        except KeyError:
+            raise ValueError(f"Unknown metric: {self.metric}.")
     
     def _k_distances(self,D,p,idx_p):
+        """Calculate the k-distance for a point p in the dataset D.
+
+        :param D: All the data points.
+        :type D: np.ndarray
+        :param p: The point for which to calculate the k-distance.
+        :type p: np.ndarray
+        :param idx_p: The index of the point p in the dataset D.
+        :type idx_p: int
+        :return: The k-distance and the indices of the k-neighbors.
+        :rtype: tuple of int and list of int
+        """
         distances = []
         for idx,o in enumerate(D):
             if idx == idx_p:
@@ -64,7 +113,15 @@ class LocalOutlierFactor(BaseEstimator, OutlierMixin):
         k_neigbors = [idx for dist,idx in distances if dist<=k_distance]
         return k_distance, k_neigbors
     
-    def _get_all_k_distances(self,D):
+    def _get_all_k_distances(self,D)->dict:
+        """Get the k-distance for all points in the dataset D.
+        This function calculates the k-distance for each point in the dataset D and caches the results.
+
+        :param D: All the data points.
+        :type D: np.ndarray
+        :return: A dictionary where the keys are the indices of the points and the values are tuples containing the k-distance and the indices of the k-neighbors.
+        :rtype: _dict
+        """
         k_distances_cache = {}
         for idx_p,p in enumerate(D):
             if idx_p in k_distances_cache:
@@ -74,9 +131,28 @@ class LocalOutlierFactor(BaseEstimator, OutlierMixin):
         return k_distances_cache
         
     def _reach_dist(self,p,o,k_distance):
+        """Calculate the reachability distance between two points p and o.
+
+        :param p: The point for which to calculate the reachability distance.
+        :type p: _np.ndarray
+        :param o: The other point.
+        :type o: _np.ndarray
+        :param k_distance: The k-distance of the point p.
+        :type k_distance: int
+        :return: The reachability distance between p and o.
+        :rtype: float
+        """
         return max(k_distance,self.metric_c(p,o))
     
-    def _local_reachability_density(self,D):
+    def _local_reachability_density(self,D)->list:
+        """Calculate the local reachability density for each point in the dataset D.
+        The local reachability density is a measure of how densely populated the local neighborhood of a point is.
+
+        :param D: All the data points.
+        :type D: np.ndarray
+        :return: A list of tuples where each tuple contains the index of the point and its local reachability density.
+        :rtype: list of tuples
+        """
         lrd = []
         k_distances = self._k_distances_cache
         for idx_p,p in enumerate(D):
@@ -94,6 +170,13 @@ class LocalOutlierFactor(BaseEstimator, OutlierMixin):
         return lrd
             
     def _local_outlier_factor(self,D):
+        """Calculate the Local Outlier Factor (LOF) for each point in the dataset D.
+
+        :param D: All the data points.
+        :type D: np.ndarray
+        :return: A list of tuples where each tuple contains the index of the point and its LOF score.
+        :rtype: _list
+        """
         lof=[]
         lrd = self._local_reachability_density(D)
         for idx_p,p in enumerate(D):
