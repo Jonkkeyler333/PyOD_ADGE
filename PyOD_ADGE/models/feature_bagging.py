0ff8a81ba03bb7342be6e12a8c934e8c6de470d2
Jonkkeyler333 <82979079+Jonkkeyler333@users.noreply.github.com>
2025-05-11 16:49:32 -0500

    feature bagging v1

--- PyOD_ADGE/models/feature_bagging.py
+++ PyOD_ADGE/models/feature_bagging.py
@@ -34,7 +34,8 @@ class FeatureBagging(BaseDetector):
         self.metric_params = metric_params
     
     def fit(self,X,y=None):
-        check_array(X)
+        X=check_array(X)
+        self.d=X.shape[0]
         ss = np.random.SeedSequence(self.random_state)
         rngs = ss.spawn(self.n_estimators)
         results=Parallel(n_jobs=self.n_jobs)(
@@ -49,9 +50,28 @@ class FeatureBagging(BaseDetector):
         return self
     
     def decision_function(self,X):
-        pass
+        check_is_fitted(self, '_is_fitted')
+        scores,idx_scores=self._breadth_first(self.estimators_scores_)
+        self.threshold_ = np.quantile(scores, 1 - self.contamination)
+        idx_sort=np.argsort(idx_scores)
+        scores=scores[idx_sort]
+        return np.where(scores>self.threshold_,-1,1)
+    
+    @property
+    def outlier_factor_(self):
+        check_is_fitted(self, '_is_fitted')
+        ASV=self.estimators_scores_
+        if self.combine == 'breadth':
+            ASV_final,idx_ASV_final=self._breadth_first(ASV)
+        else:
+            ASV_final,idx_ASV_final=self._cumulative(ASV)
+        idx_sort=np.argsort(idx_ASV_final)
+        scores=ASV_final[idx_sort]
+        return scores
     
     def get(self):
+        """Funcion de prueba , despues se eliminara
+        """
         return self.subsets_,self.estimators_scores_
 
     def _bagging_features_idx(self,d,rng)->np.ndarray:
@@ -65,3 +85,29 @@ class FeatureBagging(BaseDetector):
             X_sub = X[:, idx]
             estimator.fit(X_sub)
             return idx, estimator.outlier_scores_
+    
+    def _breadth_first(self,ASV):
+        ASV=np.stack(ASV)
+        if not ASV.shape[0] == self.n_estimators:
+            raise ValueError("el numero de estimadores no coincide con el numero de ASV")
+        if self.base_estimator == 'sklearn':
+            sort_idx_ASV=np.argsort(-ASV,axis=1)
+            sort_ASV=np.take_along_axis(ASV,sort_idx_ASV,axis=1)
+        else:
+            sort_idx_ASV=np.argsort(ASV,axis=1)
+            sort_ASV=np.take_along_axis(ASV,sort_idx_ASV,axis=1)
+        ASV_final=[]
+        idx_ASV_final=[]
+        for i in range(self.d):
+            for j in range(self.n_estimators):
+                elem=sort_ASV[j,i]
+                if sort_idx_ASV[j,i] in idx_ASV_final:
+                    continue
+                ASV_final.append(elem)
+                idx_ASV_final.append(sort_idx_ASV[j,i])
+        IndFINAL = np.array(idx_ASV_final, dtype=int)
+        ASV_final  = np.array(ASV_final, dtype=float)
+        return ASV_final,IndFINAL
+    
+    def _cumulative(self,ASV):
+        pass
