77bea05eebb545098b44d56eaa9071ce4042dfcb
Lucky <jhonandersonvargas6@gmail.com>
2025-05-13 22:50:53 -0500

    Update __init__.py
    Se agrega el algoritmo PCA al _init_

--- PyOD_ADGE/models/__init__.py
+++ PyOD_ADGE/models/__init__.py
@@ -1,5 +1,6 @@
 from .local_outlier_factor import LocalOutlierFactor
 from .base import BaseDetector
 from .feature_bagging import FeatureBagging
+from .principal_component_analysis import PCA
 
-__all__ = ["LocalOutlierFactor", "BaseDetector","FeatureBagging"]
+__all__ = ["LocalOutlierFactor", "BaseDetector","FeatureBagging","PCA"]
